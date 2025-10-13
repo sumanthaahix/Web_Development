7b058d0af1407dd4e40deca159fb05dbb1b81240
SUMANTHnew <polinenisumanth55@gmail.com>
2025-10-13 23:27:43 +0530

    new

---
+++ Web-development/JavaScript/Practice/practice.js
@@ -0,0 +1,47 @@
+
+/*
+// // alert("Hi Hello")
+// document.querySelector("h1").innerHTML="Good Bye";
+// document.getElementById("demo").innerHTML = "Hello World!";
+*/
+
+
+// // h1.innerHTML="Good Bye"
+
+// console.log(document.body);
+// console.dir(document.body);
+// console.log(document.head);
+
+// console.dir(document.body.childNodes[2]);
+
+// document.body.childNodes[3].innerText="abcd";
+
+// console.dir(document.body);
+
+
+// Select with id
+// console.log(document.getElementById("heading"));
+
+
+// let heading=document.getElementById("heading");
+// console.log(heading);
+// console.dir(heading);
+
+
+// select with class
+
+// let headings=document.getElementsByClassName("heading-class");
+// console.log(headings);
+// console.dir(headings);
+
+
+// Qyery Selector
+// let firstEle=document.querySelector("p");
+// console.dir(firstEle);
+
+// let allEle=document.querySelectorAll("p");
+// console.dir(allEle);
+
+// textContent change
+let heading=document.querySelector("h1");
+
---
+++ Web-development/JavaScript/index.js
@@ -0,0 +1,107 @@
+// alert("my name is  sumanth");
+
+
+// alert("my name is " + myname +" Welcome to my course "+yourName+"!");
+
+
+// var message="Hello ";
+// var name="sumanth";
+
+// // alert(message+name)
+// name.length
+// message.length
+
+
+
+// var tweet= prompt("Compose your tweet");
+// var tweetCount=tweet.length
+// alert("you have written "+ tweetCount + " characters ,you have " + (140-tweetCount) + " characters remaning");
+
+
+// var name="Angela";
+// name.slice(1,3);
+// name.slice(5,6);
+
+// var tweet= prompt("Compose your tweet");
+// var tweetUnder140=tweet.slice(0,140)
+// alert(tweetUnder140)
+
+// alert(prompt("Compose your tweet").slice(0,140));
+
+
+// -----------------
+
+// function test(){
+//     var a="3"
+//     var b="8"
+//     c=a
+//     a=b
+//     b=c
+    
+
+//     alert(console.log("a is "+a))
+//     alert(console.log("b is "+b))
+// }
+
+// ---------
+
+// toUppercase()
+// var name="Sumanth"
+// name=name.toUpperCase();
+// nam=name.toLowerCase();
+
+
+
+// First letter capital
+// var name=prompt("What is your name?");
+// var firstChar=name.slice(0,1);
+// var upperCaseFirstChar=firstChar.toUpperCase();
+// var restOfName=name.slice(1,name.length);
+// var capitalisedName=upperCaseFirstChar+restOfName;
+// alert("Hello " + capitalisedName)
+// capitalisedName
+
+
+
+// // suppose sumaNth  to Sumanth
+// var name=prompt("What is your name?");
+// var firstChar=name.slice(0,1);
+// var upperCaseFirstChar=firstChar.toUpperCase();
+// var restOfName=name.slice(1,name.length);
+// restOfName=restOfName.toLowerCase();
+// var capitalisedName=upperCaseFirstChar+restOfName;
+// alert("Hello " + capitalisedName)
+
+// ========
+
+// Numbers
+// console.log("sumanth")
+// alert("sumanth");
+
+
+
+// --------------------------
+//Functions 
+
+// function add(num1, num2) {
+// return num1 + num2;
+// }
+ 
+// function subtract(num1, num2) {
+// return num1 - num2;
+// }
+ 
+// function multiply(num1, num2) {
+// return num1 * num2;
+// }
+ 
+// function divide(num1, num2) {
+// return num1 / num2;
+// }
+ 
+// function calculator(num1, num2, operator) {
+// return operator(num1, num2);
+// }
+
+// calculator(4,5,add);
+// 9
